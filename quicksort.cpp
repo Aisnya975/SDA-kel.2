50ea9680d4c4cbd627a4bbaefc2fc0ce43a332bc
alpreds <155495282+ALPHARIET@users.noreply.github.com>
2025-04-27 00:13:52 +0700

    Add files via upload

---
+++ quicksort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high]; // pivot ambil elemen terakhir
+    int i = low - 1; // indeks elemen lebih kecil
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+void quicksort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quicksort(arr, low, pi - 1);
+        quicksort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    vector<int> arr = {10, 7, 8, 9, 1, 5};
+    int n = arr.size();
+
+    quicksort(arr, 0, n - 1);
+
+    cout << "Hasil setelah diurutkan: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
